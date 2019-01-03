72d3208cde7b5ba9d05b5ba4125509c2b424b62b
Jake Thurman <jacob@thurmans.com>
2019-01-03 10:35:34 -0500

    Handle ajax page reloads.
    Updating author profile pics didn't work in cases where the page is reloaded with ajax (i.e., going from code->pulls for a repo). Now it does!

--- GITHUB-show_author_profile_pic_for_pulls.user.js
+++ GITHUB-show_author_profile_pic_for_pulls.user.js
@@ -1,29 +1,42 @@
 // ==UserScript==
 // @name         GITHUB - Show author profile pics for pulls
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @downloadURL  https://github.com/JakeThurman/misc-tampermonkey/raw/master/GITHUB-show_author_profile_pic_for_pulls.user.js
 // @author       @JakeThurman
-// @match        https://github.com/**/pulls**
-// @match        https://github.com/pulls**
+// @match        https://github.com/**
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var parseUrl = queryString => JSON.parse('{"' + decodeURI(queryString.replace(/&/g, "\",\"").replace(/=/g,"\":\"")) + '"}');
+    function showAuthorProfilePics() {
+        if (!location.pathname.toLowerCase().endsWith("/pulls") && !location.pathname.toLowerCase().endsWith("/pulls/"))
+            return;
 
-    document.querySelectorAll(".opened-by a").forEach(function (author) {
-        var url = author.getAttribute("data-hovercard-url");
-        var queryString = url.split("?")[1];
-        var params = parseUrl(queryString);
+        var parseUrl = queryString => JSON.parse('{"' + decodeURI(queryString.replace(/&/g, "\",\"").replace(/=/g,"\":\"")) + '"}');
 
-        var id = params.user_id;
-        var name = author.innerText;
+        document.querySelectorAll(".opened-by a").forEach(function (author) {
+            var url = author.getAttribute("data-hovercard-url");
+            var queryString = url.split("?")[1];
+            var params = parseUrl(queryString);
 
-        var image = '<img class="from-avatar" src="https://avatars2.githubusercontent.com/u/' + id + '?s=40" width="20" height="20" alt="@' + name + '">';
-        author.innerHTML = "@" + name + " " + image;
-    });
+            var id = params.user_id;
+            var name = author.innerText;
+
+            var image = '<img class="from-avatar" src="https://avatars2.githubusercontent.com/u/' + id + '?s=40" width="20" height="20" alt="@' + name + '">';
+            author.innerHTML = "@" + name + " " + image;
+        });
+    }
+
+    // When the content of the body changes, try to show the author pics (if we're on the right page)
+    var observer = new MutationObserver(showAuthorProfilePics);
+
+    var toReload = document.getElementById("js-repo-pjax-container");
+    observer.observe(toReload, { childList: true });
+
+    // Try and show right away
+    showAuthorProfilePics();
 })();
