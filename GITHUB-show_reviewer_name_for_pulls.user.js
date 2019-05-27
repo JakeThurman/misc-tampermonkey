6627b60fa367ba46f8c2fd0ea7135b25c681b1ef
Jake Thurman <jacob@thurmans.com>
2019-05-27 09:52:33 -0400

    Add github script to show reviewer name.

---
+++ GITHUB-show_reviewer_name_for_pulls.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         GITHUB - Show reviewer name for pulls
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @downloadURL  https://github.com/JakeThurman/misc-tampermonkey/raw/master/GITHUB-show_reviewer_name_for_pulls.user.js
+// @author       @JakeThurman
+// @match        https://github.com/**
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function showAuthorProfilePics() {
+        if (!location.pathname.toLowerCase().endsWith("/pulls") && !location.pathname.toLowerCase().endsWith("/pulls/")) {
+            return;
+        }
+        var getQueryString = url => JSON.parse('{"' + decodeURI((url.split("?")[1] || "").replace(/&/g, "\",\"").replace(/=/g,"\":\"")) + '"}');
+
+        document.querySelectorAll(".avatar .from-avatar").forEach(function (reviewer) {
+            var name = " " + reviewer.getAttribute("alt").replace("@", "");
+
+            var textNode = document.createTextNode(name);
+            reviewer.parentNode.insertBefore(textNode, reviewer);
+
+            // Make width automatic
+            reviewer.parentNode.style.width = "auto";
+        });
+    }
+
+    // When the content of the body changes, try to show the author pics (if we're on the right page)
+    var observer = new MutationObserver(showAuthorProfilePics);
+
+    var toReload = document.getElementById("js-repo-pjax-container");
+    observer.observe(toReload, { childList: true });
+
+    // Try and show right away
+    showAuthorProfilePics();
+})();
