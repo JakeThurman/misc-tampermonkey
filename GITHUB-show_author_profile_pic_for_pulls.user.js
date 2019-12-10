5643c012b3cc85e31316294c7c4575fb6739e6f8
Jake Thurman <jacob@thurmans.com>
2019-12-10 10:03:43 -0500

    Fix Github author pic script
    This broke because github updated their hovercard url. 
    Makes sense- It was an internal API.
    
    I also should have just been using the public url for profile pics.

--- GITHUB-show_author_profile_pic_for_pulls.user.js
+++ GITHUB-show_author_profile_pic_for_pulls.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GITHUB - Show author profile pics for pulls
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  try to take over the world!
 // @downloadURL  https://github.com/JakeThurman/misc-tampermonkey/raw/master/GITHUB-show_author_profile_pic_for_pulls.user.js
 // @author       @JakeThurman
@@ -16,17 +16,13 @@
         if (!location.pathname.toLowerCase().endsWith("/pulls") && !location.pathname.toLowerCase().endsWith("/pulls/"))
             return;
 
-        var getQueryString = url => JSON.parse('{"' + decodeURI((url.split("?")[1] || "").replace(/&/g, "\",\"").replace(/=/g,"\":\"")) + '"}');
-
         document.querySelectorAll(".opened-by a").forEach(function (author) {
-            var url = author.getAttribute("data-hovercard-url");
-            var params = getQueryString(url);
+            // It's okay if this runs multiple times by accident, since the
+            //  inner text is still just the username in the updated state.
+            var username = author.innerText;
 
-            var id = params.user_id;
-            var name = author.innerText;
-
-            var image = '<img class="from-avatar" src="https://avatars2.githubusercontent.com/u/' + id + '?s=40" width="20" height="20" alt="@' + name + '">';
-            author.innerHTML = name + " " + image;
+            var image = '<img class="from-avatar" src="https://github.com/' + username + '.png?size=40" width="20" height="20" alt="@' + username + '\'s profile picture">';
+            author.innerHTML = username + " " + image;
         });
     }
 
